546c6fcee15acd70c017348447f3a8aee8d02b55
Tùng Đỗ <kristensendo.26@gmail.com>
2025-12-10 20:35:16 +0700

    fix label Chon Fs

--- src/pages/Contract/QuoteModal/QuoteModal.tsx
+++ src/pages/Contract/QuoteModal/QuoteModal.tsx
@@ -368,7 +368,7 @@ export default function QuoteModal(props: any) {
         <SelectCustom
           id="fsId"
           name="fsId"
-          label="Chọn FssssS"
+          label="Chọn Fs"
           options={[]}
           fill={true}
           value={valueFS}
