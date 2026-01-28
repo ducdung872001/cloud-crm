74b92ffa8d73f8b64397a48064fa259cd40c6783
Nguyen-Dinh-Luyen <114087826+Nguyen-Dinh-Luyen@users.noreply.github.com>
2026-01-28 11:02:37 +0700

    Fix 356 và sửa lại tên cài đặt quy trình

--- src/pages/PartnerList/PartnerList.tsx
+++ src/pages/PartnerList/PartnerList.tsx
@@ -668,7 +668,7 @@ export default function PartnerList() {
 
   const defaultFieldCustomer = [
     { id: 1, name: "Email", fieldName: "emailMasked", isTable: false },
-    { id: 3, name: "Người đại diện pháp luật", fieldName: "contactId", isTable: false },
+    { id: 3, name: "Người đại diện pháp luật", fieldName: "contactName", isTable: false },
   ];
 
   const defaultFieldTableDis = [
